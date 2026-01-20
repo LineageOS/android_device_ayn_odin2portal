7ebd7fa2ef6e3a994f7e3a8974d57c3dd2b61231
AnierinB <anierin@evolution-x.org>
2026-01-20 04:17:27 -0800

    odin2portal: Initial skeleton tree
    Change-Id: Ic1331056aa058487059f0880bf1f2edb9ad10bfe
    Signed-off-by: AnierinB <anierin@evolution-x.org>

---
+++ lineage_odin2portal.mk
@@ -0,0 +1,23 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit from odin2portal device
+$(call inherit-product, device/ayn/odin2portal/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+
+PRODUCT_NAME := lineage_odin2portal
+PRODUCT_DEVICE := odin2portal
+PRODUCT_MANUFACTURER := AYN
+PRODUCT_BRAND := AYN
+PRODUCT_MODEL := Odin2 Portal
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DeviceProduct=odin2portal
