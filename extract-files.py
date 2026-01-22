9d5bba77f5cfaa064638bd43aeafc9df2e21d16d
AnierinB <anierin@evolution-x.org>
2026-01-22 10:24:48 +0000

    odin2portal: Initial vendor blobs
    Change-Id: Iebad0186b44fdb2db127ae2e35005f63a744013f
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- extract-files.py
+++ extract-files.py
@@ -4,6 +4,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+
+from extract_utils.fixups_lib import (
+    lib_fixups,
+    lib_fixups_user_type,
+)
+
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
@@ -13,10 +24,22 @@ namespace_imports = [
     'vendor/ayn/qcs8550-common',
 ]
 
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    'vendor/etc/display/qdcm_calib_data_icna3512_amoled_panel_with_DSC.json': blob_fixup()
+        .regex_replace('ar07_7inch_cmd_mode_dsi_panel', 'icna3512_amoled_panel_with_DSC'),
+}  # fmt: skip
+
 module = ExtractUtilsModule(
     'odin2portal',
     'ayn',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
+    add_firmware_proprietary_file=True,
 )
 
 if __name__ == '__main__':
