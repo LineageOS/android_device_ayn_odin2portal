f61dd27197c26eaf3d671a29fbe42853d418817e
AnierinB <anierin@evolution-x.org>
2026-08-11 08:30:30 +0000

    odin2portal: Build charger_rgb
    Change-Id: Ifcef0bb1296c2ca0450b18338754769d239c7b78
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- device.mk
+++ device.mk
@@ -6,6 +6,10 @@
 # AAPT
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# Charger
+PRODUCT_PACKAGES += \
+    charger_rgb
+
 # IDC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/idc/fts_ts.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/fts_ts.idc
